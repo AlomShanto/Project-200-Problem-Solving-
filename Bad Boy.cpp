87174f5110900b38f3b41dd255c1d4ba146c290b
AlomShanto <73125478+AlomShanto@users.noreply.github.com>
2021-06-24 20:09:47 +0600

    Add files via upload
    Codeforces Round #726 (Div. 2), Problem B, Bad Boy

---
+++ Bad Boy.cpp
@@ -0,0 +1,16 @@
+///BISMILLAHIR-RAHMANIR-RAHIM///
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define srt(s) sort(s.begin(),s.end())
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m,i,j;
+        cin>>n>>m>>i>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+}
