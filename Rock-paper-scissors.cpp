e6d0671e1135926993449c262d00a127ec5d9655
Md. Shuhayel Alom Shanto <73125478+AlomShanto@users.noreply.github.com>
2021-06-28 13:30:47 +0600

    Add files via upload
    AtCoder Beginner Contest 204, Problem A, Rock-paper-scissors

---
+++ Rock-paper-scissors.cpp
@@ -0,0 +1,18 @@
+///BISMILLAHIR-RAHMANIR-RAHIM///
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define srt(s) sort(s.begin(),s.end())
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+    if(a==b)
+    {
+        cout<<a<<endl;
+    }
+    else
+    {
+        cout<<3-a-b<<endl;
+    }
+}
