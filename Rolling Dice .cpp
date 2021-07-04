3358fa473c3ee9a0f8acd98a86fb3798f5dbc391
Md. Shuhayel Alom Shanto <73125478+AlomShanto@users.noreply.github.com>
2021-07-04 20:39:59 +0600

    Add files via upload
    AtCoder Beginner Contest 208, Problem A, Rolling Dice

---
+++ Rolling Dice .cpp
@@ -0,0 +1,19 @@
+///BISMILLAHIR-RAHMANIR-RAHIM///
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define srt(s) sort(s.begin(),s.end())
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+    if(a*6>=b && a<=b)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+}
+
