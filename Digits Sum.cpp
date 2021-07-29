0494b79901ba82a886aaaff84a5a1822f8e3fae8
Md. Shuhayel Alom Shanto <73125478+AlomShanto@users.noreply.github.com>
2021-07-29 11:25:01 +0600

    Add files via upload
    Harbour.Space Scholarship Contest 2021-2022 (open for everyone, rated, Div. 1 + Div. 2), Problem A, Digits Sum

---
+++ Digits Sum.cpp
@@ -0,0 +1,16 @@
+///BISMILLAHIR-RAHMANIR-RAHIM///
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define srt(s) sort(s.begin(),s.end())
+int main()
+{
+    ll t=1;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        cout<<(n+1)/10<<endl;
+    }
+}
